3d90bb039c20464adc9ff168939a15a99d9974e5
Andrew Kim <akim618@gmail.com>
2018-12-27 04:18:27 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -12,7 +12,7 @@ def fourth_challenge
         {name: "Mercutio",
          age: "18",
          attitude: "hot-headed"}      ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
@@ -28,6 +28,6 @@ def fourth_challenge
    }
 }
 
-  
+
 
 end
