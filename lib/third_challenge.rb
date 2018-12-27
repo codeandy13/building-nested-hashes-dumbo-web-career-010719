ec0f732dcd41db815df1af5a6f234f9ea79575b8
Andrew Kim <akim618@gmail.com>
2018-12-27 04:05:47 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -46,8 +46,7 @@ def third_challenge
           }
           {  name: "Nurse",
              age: "44",
-             attitude: "worried"
-        }
+             attitude: "worried"}
         ]
    }
   }
