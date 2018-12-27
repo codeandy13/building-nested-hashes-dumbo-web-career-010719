3995e591092f16dfcd095fb37182fee668a126f0
Andrew Kim <akim618@gmail.com>
2018-12-27 03:25:07 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,8 +5,8 @@ def second_challenge
       :matriarch => {},
       :hero => {},
       :hero_friends => {[
-        
-        ]}
+
+        ]},
       },
     :capulet => {
       #your key/value pairs here!
